56162a00081c067b9b774cbc5ab6ee89527acfdd
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-01-20 14:32:11 +0900

    とりあえず、名前空間でPlayerを継承させて、カメラアングルを調整する

---
+++ FPSGunAct/Assets/Script/CamearaAct/CameraManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+    RequireComponent(typeof(Camera))]
+    public class CameraManager : MonoBehaviour
+    {
+        public Camera _camera;
+
+        
+        void CameraWork_Pod()
+        {
+
+        }
+
+        void CameraWork_Player()
+        {
+
+        }
+        
+        void CameraWork_Event()
+        {
+
+        }
+    }
+
+
