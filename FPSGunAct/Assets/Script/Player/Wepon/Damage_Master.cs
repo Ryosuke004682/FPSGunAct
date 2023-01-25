320f7bbc0366b6cb09c97bfe67f3d4b815b04851
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-01-26 02:19:06 +0900

    攻撃判定が入るように調整。 次は、相手にノックバックを入れる。

---
+++ FPSGunAct/Assets/Script/Player/Wepon/Damage_Master.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class Damage_Master : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Hit()
+    {
+ 
+    }
+
+    
+
+}
