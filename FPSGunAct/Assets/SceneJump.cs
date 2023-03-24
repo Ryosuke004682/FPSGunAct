8629497723ac46262f0511bc567174070c60c55a
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-03-25 04:00:52 +0900

    スポーンはできたけど位置がおかしいので修正中

--- FPSGunAct/Assets/SceneJump.cs
+++ FPSGunAct/Assets/SceneJump.cs
@@ -6,6 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneJump : MonoBehaviour
 {
+    //DontDestroyOnLoad()で消さずにスポーンさせることができる
+    /*スポーンしたときに任意のポジションにスポーンしてほしい。*/
+
+    [SerializeField, Header("ステージ遷移後も、ステータスを保持したいやつ")] 
+    private GameObject[] saveObjects;
+
+    [SerializeField, Header("スポーンの位置")]
+    private GameObject playerSpawnPosition;
+
+
+    public void Start()
+    {
+        for (var i = 0; i < saveObjects.Length; i++)
+        {
+            DontDestroyOnLoad(saveObjects[i]);
+        }
+    }
+
+
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
@@ -18,8 +37,21 @@ public class SceneJump : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Return))
         {
-            SceneManager.LoadScene("CamingSoon");
+            SceneManager.LoadScene("MainScene");
+            StartCoroutine(Spawn());
         }
     }
 
+    private IEnumerator Spawn()
+    {
+        playerSpawnPosition = GameObject.Find("SpawnObject");
+        UnityEngine.Debug.Log("(コルーチン)探せてるよ" + playerSpawnPosition);
+
+        for(var i = 0; i < saveObjects.Length; i++)
+        {
+            playerSpawnPosition = saveObjects[i];
+        }
+
+        yield return null;
+    }
 }
